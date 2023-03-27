cc63a1ee0e26b66d4ba3afd7b559ac8ef7b2c2f8
tarasinski.dawid <tarasinski.dawid@STACJA105-05>
2023-03-27 13:30:40 +0200

    Dodaj pliki projektów.

---
+++ WpfApp2/visualizacja/wpf_3.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2.visualizacja
+{
+    /// <summary>
+    /// Logika interakcji dla klasy wpf_3.xaml
+    /// </summary>
+    public partial class wpf_3 : Window
+    {
+        public wpf_3()
+        {
+            InitializeComponent();
+        }
+    }
+}
